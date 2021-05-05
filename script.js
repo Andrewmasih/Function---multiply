4671a1140daba2d64388df4d125d01086bd5f386
Andrew Masih <armasih@outlook.com>
2021-05-05 16:10:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function multiply (a, b)  {
+  // the function takes two parameters 'a' and 'b'
+
+  // and returns the product
+  return a*b;        
+  
+} 
+
+multiply (10, 2);
+
+// call the multiply function with passing the argument 10 and 2.
+
+// 10 becomes the value of 'a' and '2' becomes the value of 'b'
+
+// try this with any number
+
